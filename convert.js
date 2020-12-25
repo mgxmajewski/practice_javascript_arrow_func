134f2c3020f1c4f3a8f0e88da936709a21e8d4af
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2020-12-25 15:38:04 +0100

    Refactor functions to arrow functions in convert.js

--- convert.js
+++ convert.js
@@ -1,12 +1,8 @@
-function addToTen(num) {
-  return 10 + num;
-}
+const addToTen = num => 10 + num;
 
-function divideUs(num1, num2) {
-  return num1/num2;
-}
+const divideUs = (num1, num2)  => num1/num2;
 
-function printMyName() {
-  let myName = 'Ashley';
-	console.log(myName);
+const printMyName = () => {
+    let myName = 'Ashley';
+    console.log(myName);
 }
